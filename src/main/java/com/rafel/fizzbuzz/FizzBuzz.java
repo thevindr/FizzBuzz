6d6f9b5dfcf02289417c77553b3cd83d9ef33489
thevindr <rafel-bel@hotmail.com>
2018-09-28 12:22:11 +0200

    [GREEN]create fizzbuzz class and fizzbuzz test

---
+++ src/main/java/com/rafel/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,4 @@
+package com.rafel.fizzbuzz;
+
+public class FizzBuzz {
+}
---
+++ src/test/java/com/rafel/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,16 @@
+package com.rafel.fizzbuzz;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class FizzBuzzTest {
+    @Test
+    public void firstTest(){
+        assertTrue(true);
+}
+
+
+
+
+}
